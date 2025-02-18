15059ceec0ce2a59751ae90659f6c34940f449d6
wittieee <jaruporn.ang@gmail.com>
2025-02-19 00:17:16 +0700

    update

---
+++ client/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function page() {
+  return (
+    <main>
+      <Link href="/register">Go Register</Link>
+    </main>
+  )
+}

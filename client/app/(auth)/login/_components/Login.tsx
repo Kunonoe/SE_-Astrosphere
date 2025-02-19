20d9b211d93f75dbe102e58eb172666d0290bdf8
LeeNxxx <pitchayanin1806@gmail.com>
2025-02-19 16:36:39 +0700

    login

--- client/app/(auth)/login/_components/Login.tsx
+++ client/app/(auth)/login/_components/Login.tsx
@@ -9,24 +9,28 @@ export default function page() {
   return (
     <main className='w-full h-screen flex justify-around items-center'>
         <Image src={Pic2} alt = "Pic2" width={400} height={400}></Image>
-        <div className=' h-fit flex flex-col items-center justify-center'>
+        <div className="h-fit flex flex-col items-center justify-center bg-white/10 backdrop-blur-lg p-10 rounded-2xl shadow-lg"> 
             <span className='text-[50px] font-semibold text-white'>Login</span>
             <div className='flex flex-col gap-5 mt-10'>
                 <div className='flex flex-col gap-1'>
                     <span className='text-white'>Username : {username}</span>
                     <input type="text" name="username" placeholder='username' className=' w-[300px] rounded-md p-2'
+                    className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
                     value={username}
                     onChange={(e)=>SetUsername(e.target.value)}
                     />
                 </div>
+                
                 <div className='flex flex-col gap-1'>
                     <span className='text-white'>Password : {password}</span>
-                    <input type="password" name="password" placeholder='password' className='w-[300px] rounded-md p-2'
-                     value={password}
+                    <input type="password" name="password" placeholder='password' 
+                    className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
+                    value={password}
                     onChange={(e)=>SetPassword(e.target.value)}
                     />
                 </div>
             </div>
+            
             <div className='flex justify-between items-center gap-5 mt-2 text-white'>
                 <span>Don't have an account?</span>
                 <span>Forgot password?</span>

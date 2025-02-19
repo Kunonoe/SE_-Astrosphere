8ff315beb868024713dcd1bc7dae2befc9459bb5
Kunonoe <tennowin@gmail.com>
2025-02-19 12:37:43 +0700

    database

--- server/src/Controllers/login.ts
+++ server/src/Controllers/login.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Account } from "../models/login";
 
 export const testData = async (req: express.Request, res: express.Response) => {
     try{
@@ -18,6 +19,56 @@ export const login = async (req: express.Request, res: express.Response) => {
         return res.send({
             status:"saccess",
             name:name
+        })
+
+    }catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+export const register = async (req: express.Request, res: express.Response) => {
+    try{
+
+        const {name,password,email}=req.body //รับพารามิเตอร์จากหน้าบ้าน มาคำนวน
+        const result = await new Account({ //await รันทั้งหน้า
+            username:name,
+            password:password,
+            email:email
+        }).save(); 
+        return res.send({ //ถ้าเซฟได้ให้้ส่งกลับไป
+           result:result
+        })
+
+    }catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+export const getuser = async (req: express.Request, res: express.Response) => {
+    try{
+
+        const {name}=req.body //รับพารามิเตอร์จากหน้าบ้าน มาคำนวน
+        const result = await Account.find({ //หา
+            username :name // หาuser = name เหมือน select
+        }) 
+        return res.send({ //ถ้าเซฟได้ให้้ส่งกลับไป
+           result:result
+        })
+
+    }catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+export const update = async (req: express.Request, res: express.Response) => {
+    try{
+
+        const {username,newpassword}=req.body //รับพารามิเตอร์จากหน้าบ้าน มาคำนวน
+        const result = await Account.updateOne({ //หา
+            username : username // หาuser = name เหมือน select
+        },{$set: { password: newpassword }})
+        return res.send({ //ถ้าเซฟได้ให้้ส่งกลับไป
+           result:result
         })
 
     }catch (error) {
--- server/src/Router/login.ts
+++ server/src/Router/login.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { testData, login } from "../controllers/login";
+import { testData, login, register, getuser, update } from "../controllers/login";
 
 export default (router: express.Router) =>{
 
     router.get('/product',testData)//ดึงข้อมูลมา
     router.post('/login',login)//ส่งข้อมูลไปที่ดาต้าเบส หลังบ้าน 
+    router.post('/register',register)
+    router.post('/getuser',getuser)
+    router.post('/update',update)
 //router.put() อัปเดตข้อมูล
 }

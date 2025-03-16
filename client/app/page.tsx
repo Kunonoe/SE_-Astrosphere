b70ca819b887e1cb71fd14f0d759bb2858c614b7
LeeNxxx <pitchayanin1806@gmail.com>
2025-03-17 00:06:32 +0700

    bg

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,7 +5,7 @@ import Tarot from "./tarot";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex justify-center items-center overflow-hidden">
+    <div className="w-full h-full flex justify-center items-center overflow-hidden bg-black">
       <main className="w-full h-[100vh] flex justify-around items-center relative px-20">
         <div className="flex items-center w-full h-full bg-cover bg-center relative">
 

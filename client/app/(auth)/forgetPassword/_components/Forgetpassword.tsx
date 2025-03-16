e41336de6da47bd386c77c8c8d3c51d85bd79d14
Kunonoe <tennowin@gmail.com>
2025-03-16 23:44:42 +0700

    newss

--- client/app/(auth)/forgetPassword/_components/Forgetpassword.tsx
+++ client/app/(auth)/forgetPassword/_components/Forgetpassword.tsx
@@ -1,18 +1,18 @@
 "use client";
-import { useState } from 'react'
-import Image from 'next/image'
-import Logo from "@/assets/logo.png"
+import { useState } from 'react';
+import Image from 'next/image';
+import Logo from "@/assets/logo.png";
 
-export default function page() {
-    const [email, SetEmail] = useState<string>("");
-    const [otp, SetOtp] = useState<string>("");
-    const [password, SetPassword] = useState<string>("");
-    const [confirmpassword, SetconfirmPassword] = useState<string>("");
+export default function ForgetPassword() {
+    const [email, setEmail] = useState<string>("");
+    const [otp, setOtp] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confirmpassword, setConfirmPassword] = useState<string>("");
 
     return (
         <div className='w-full h-[90vh] flex justify-center items-center'>
             <main className='w-full h-full flex justify-around items-center'>
-                <Image src={Logo} alt="Pic2" width={350} height={350}></Image>
+                <Image src={Logo} alt="Logo" width={350} height={350} />
 
                 <div className="h-fit flex flex-col items-center justify-center bg-white/10 backdrop-blur-lg p-10 rounded-2xl shadow-lg">
                     <span className='text-[40px] font-semibold text-white'>Forgot Password</span>
@@ -22,10 +22,10 @@ export default function page() {
                         <div className='w-full flex justify-start items-center gap-5'>
                             <div className='flex flex-col gap-1'>
                                 <span className='text-white'>Email : </span>
-                                <input type="text" name="Enter your email" placeholder='Enter your email'
+                                <input type="text" name="email" placeholder='Enter your email'
                                     className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
                                     value={email}
-                                    onChange={(e) => SetEmail(e.target.value)}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
                             <button className='px-5 py-2 bg-emerald-600 text-white font-bold rounded-lg mt-5'>Send</button>
@@ -34,10 +34,10 @@ export default function page() {
                         {/* OTP */}
                         <div className='flex flex-col gap-1'>
                             <span className='text-white'>OTP : </span>
-                            <input type="text" name="Enter your OTP" placeholder='Enter your OTP'
+                            <input type="text" name="otp" placeholder='Enter your OTP'
                                 className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
                                 value={otp}
-                                onChange={(e) => SetOtp(e.target.value)}
+                                onChange={(e) => setOtp(e.target.value)}
                             />
                         </div>
 
@@ -48,17 +48,17 @@ export default function page() {
                                 <input type="password" name="password" placeholder='password'
                                     className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
                                     value={password}
-                                    onChange={(e) => SetPassword(e.target.value)}
+                                    onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
 
                             {/* Confirm Password */}
                             <div className='flex flex-col gap-1'>
                                 <span className='text-white'>Confirm Password : </span>
-                                <input type="password" name="confirm password" placeholder='confirm password'
+                                <input type="password" name="confirmPassword" placeholder='confirm password'
                                     className="w-[300px] rounded-md p-2 bg-white/20 backdrop-blur-md text-white placeholder-gray-300"
                                     value={confirmpassword}
-                                    onChange={(e) => SetconfirmPassword(e.target.value)}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -66,8 +66,7 @@ export default function page() {
 
                     <button className='px-5 py-2 bg-emerald-600 text-white font-bold rounded-lg mt-5'>Confirm</button>
                 </div>
-
             </main>
         </div>
-    )
-    }
+    );
+}

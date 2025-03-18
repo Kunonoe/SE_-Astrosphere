d41bec350dc58c9067e626bb6b9233c6fc97b35a
Kunonoe <tennowin@gmail.com>
2025-03-18 12:23:28 +0700

    news

--- server/src/Router/message.ts
+++ server/src/Router/message.ts
@@ -1,5 +1,4 @@
 import { receiveContent } from "../Controllers/message"; // ฟังก์ชันบันทึกข้อความ
-import { sendScheduledMessages } from "../SubServer/sendmessages"; // ฟังก์ชันส่งข้อความ
 import express from "express";
 
 export default (router: express.Router) => {
--- server/src/SubServer/croncounttime.ts
+++ server/src/SubServer/croncounttime.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { sendScheduledMessages } from "./sendmessages";
 
 // ✅ ตั้ง Cron Job ให้รันทุกๆ 10 นาที
-cron.schedule("*/10 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
     console.log("🔔 Running scheduled message sender every 5 minutes...");
     await sendScheduledMessages();
 });

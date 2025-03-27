08bb6923b4e4c48c9168c4ceb2cf323d2e938fbe
Kunonoe <tennowin@gmail.com>
2025-03-28 00:54:00 +0700

    testt

--- server/src/Controllers/message.ts
+++ server/src/Controllers/message.ts
@@ -3,7 +3,7 @@ import { Message } from "../models/message";
 
 // กำหนดเวลาส่งข้อความเป็น เที่ยงคืน 0 นาที (00:00 น.)
 const DEFAULT_SEND_HOUR = 0;  // เที่ยงคืน
-const DEFAULT_SEND_MINUTE = 0; // 0 นาที
+const DEFAULT_SEND_MINUTE = 57; // 0 นาที
 
 export const receiveContent = async (req: express.Request, res: express.Response) => {
     try {
--- server/src/SubServer/croncounttime.ts
+++ server/src/SubServer/croncounttime.ts
@@ -3,7 +3,7 @@ import { sendScheduledMessages } from "./sendmessages";
 
 
 // ตั้ง Cron Job ให้รันทุกๆ เที่ยงคืน 1 นาที "* * * * *" เทส ทุกๆ1นาที
-cron.schedule("1 0 * * *", async () => {
+cron.schedule("* * * * *", async () => {
     console.log("🔔 Running scheduled message sender once a day at 00:01...");
     await sendScheduledMessages();
 });
